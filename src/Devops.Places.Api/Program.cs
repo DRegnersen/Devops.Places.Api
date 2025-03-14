3be1b46af3b0702d990b73905b27cc19a0e40b59
Dennis Regnersen <deofrivia@gmail.com>
2025-03-14 20:14:13 +0300

    use production env in docker-compose.yml

--- src/Devops.Places.Api/Program.cs
+++ src/Devops.Places.Api/Program.cs
@@ -12,11 +12,8 @@ builder.Services.AddDataAccess(configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMongoDbInitialization();
 app.UseHttpsRedirection();
